d08363d02e8e8469a00d9d61f4150af4dd777d81
Vallin Plasencia Valdes <vallin.plasencia@gmail.com>
2018-09-28 10:57:07 -0400

    Actualizando el commit.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
 
     <!-- Custom styles for this template -->
     <link href="{{asset('css/dashboard.css')}}" rel="stylesheet">
+
+    <!--Iconos Font Awesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 
 <body>
@@ -31,74 +34,66 @@
     <div class="row">
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
             <div class="sidebar-sticky">
-                <ul class="nav flex-column">
+                {{--<ul class="nav flex-column">--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a class="nav-link active" href="#">--}}
+                            {{--<span data-feather="home"></span>--}}
+                            {{--Dashboard <span class="sr-only">(current)</span>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a class="nav-link" href="#">--}}
+                            {{--<span data-feather="file"></span>--}}
+                            {{--Orders--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                 {{----}}
+                {{--</ul>--}}
+
+                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                    <span>TIPO</span>
+                    {{--<a class="d-flex align-items-center text-muted" href="#">--}}
+                        {{--<i class="fas fa-plus"></i>--}}
+                    {{--</a>--}}
+                </h6>
+                <ul class="nav flex-column mb-2">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">
-                            <span data-feather="home"></span>
-                            Dashboard <span class="sr-only">(current)</span>
+                        <a class="nav-link" href="#">
+                           <i class="fas fa-list"></i> Listar tipos
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">
-                            <span data-feather="file"></span>
-                            Orders
+                           <i class="fas fa-plus"></i> Nuevo tipo
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">
-                            <span data-feather="shopping-cart"></span>
-                            Products
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <span data-feather="users"></span>
-                            Customers
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <span data-feather="bar-chart-2"></span>
-                            Reports
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <span data-feather="layers"></span>
-                            Integrations
+                           <i class="fas fa-search"></i> Buscar
                         </a>
                     </li>
                 </ul>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>Saved reports</span>
-                    <a class="d-flex align-items-center text-muted" href="#">
-                        <span data-feather="plus-circle"></span>
-                    </a>
+                    <span>AUTOS</span>
+                    {{--<a class="d-flex align-items-center text-muted" href="#">--}}
+                    {{--<i class="fas fa-plus"></i>--}}
+                    {{--</a>--}}
                 </h6>
                 <ul class="nav flex-column mb-2">
                     <li class="nav-item">
                         <a class="nav-link" href="#">
-                            <span data-feather="file-text"></span>
-                            Current month
+                            <i class="fas fa-list"></i> Listar autos
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">
-                            <span data-feather="file-text"></span>
-                            Last quarter
+                            <i class="fas fa-plus"></i> Nuevo auto
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">
-                            <span data-feather="file-text"></span>
-                            Social engagement
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <span data-feather="file-text"></span>
-                            Year-end sale
+                            <i class="fas fa-search"></i> Buscar
                         </a>
                     </li>
                 </ul>
@@ -106,20 +101,7 @@
         </nav>
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">Dashboard</h1>
-                <div class="btn-toolbar mb-2 mb-md-0">
-                    <div class="btn-group mr-2">
-                        <button class="btn btn-sm btn-outline-secondary">Share</button>
-                        <button class="btn btn-sm btn-outline-secondary">Export</button>
-                    </div>
-                    <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                        <span data-feather="calendar"></span>
-                        This week
-                    </button>
-                </div>
-            </div>
-
+            @yield('content')
         </main>
     </div>
 </div>
@@ -131,11 +113,6 @@
 <script src="{{asset('libs/popper.min.js')}}"></script>
 <script src="{{asset('libs/bootstrap-4.1.3/js/bootstrap.min.js')}}"></script>
 
-<!-- Icons -->
-<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-<script>
-    feather.replace()
-</script>
 
 </body>
 </html>
